26c695ba5c571372361dcb876f9b64ba78b28b9b
zicla <lish516@126.com>
2017-10-20 14:46:43 +0800

    Study chapter3.

--- chapter2/mian.py
+++ chapter2/mian.py
@@ -1,7 +1,42 @@
 from recommendations import critics
 from recommendations import sim_distance
+from recommendations import sim_pearson
 from math import sqrt
+
+
+# Gets recommendations for a person by using a weighted average
+# of every other user's rankings
+def getRecommendations(prefs, person, similarity=sim_pearson):
+    totals = {}
+    simSums = {}
+    for other in prefs:
+        # don't compare me to myself
+        if other == person: continue
+        sim = similarity(prefs, person, other)
+
+        # ignore scores of zero or lower
+        if sim <= 0: continue
+        for item in prefs[other]:
+
+            # only score movies I haven't seen yet
+            if item not in prefs[person] or prefs[person][item] == 0:
+                # Similarity * Score
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                # Sum of similarities
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+
+    # Create the normalized list
+    rankings = [(total / simSums[item], item) for item, total in totals.items()]
+
+    # Return the sorted list
+    rankings.sort()
+    rankings.reverse()
+    return rankings
 
 if __name__ == '__main__':
     d = sim_distance(critics, 'Lisa Rose', 'Claudia Puig')
+    print d
+    d = getRecommendations(critics,'Toby')
     print d
